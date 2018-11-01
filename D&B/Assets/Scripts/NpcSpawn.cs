7075455cb4b532b7a13dcbb3519c1bbd0eea9dfa
Postboredom <noah1010@me.com>
2018-11-01 13:03:18 -0700

    work

--- D&B/Assets/Scripts/NpcSpawn.cs
+++ D&B/Assets/Scripts/NpcSpawn.cs
@@ -2,7 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcSpawn : MonoBehaviour {
-    public int transform;
+public class NpcSpawn : MonoBehaviour
+{
+    private GameObject[] chars;
+    public GameObject controller;
+    private void Awake()
+    {
+        chars = GameObject.FindGameObjectsWithTag("NPC");
+    }
+    void SpawnNPCrandom()
+    {
+        GameObject newchar;
 
+        newchar = chars[Random.Range(0, chars.Length - 1)];
+        newchar.transform.parent = controller.transform;
+        newchar.AddComponent<Charsheet>();
+
+        Instantiate(controller);
+    }
+
+    void SpawnNPC(string type)
+    {
+        Debug.Log("Work in Progress");
+    }
 }
