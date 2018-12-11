3ddb7e65219c538e6c328c8811b4ece2aa68cfcb
Postboredom <noah1010@me.com>
2018-12-11 01:35:37 -0800

    Quick Updates

---
+++ DBF/Assets/Scripts/Markov.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Markov : MonoBehaviour {
+    public int levels;
+    public List<List<MarkovNodes<Dialouge_nodes>>> diaTree = new List<List<MarkovNodes<Dialouge_nodes>>>();
+    public List<MarkovNodes<Dialouge_nodes>> dialogue = new List<MarkovNodes<Dialouge_nodes>>();
+    public MarkovNodes<Dialouge_nodes> nodes = new MarkovNodes<Dialouge_nodes>();
+    private void Awake()
+    {
+        assignlevel();
+        filltree();
+        createFlowGraph();
+        Debug.Log(displayGraph().ToString());
+    }
+
+    public string displayGraph()
+    {
+        string print = "";
+        for(int ii = 0; ii< diaTree.Count;ii++)
+        {
+            print = print + "Level " + ii + 1 + " nodes: " + "\n";
+            for (int nodecount = 0; nodecount < diaTree[ii].Count; nodecount++)
+            {
+                print = print + "\t" + diaTree[ii][nodecount].ToString() + "\n";
+                    }
+        }
+        return print;
+    }
+
+    public void createFlowGraph()
+    {
+        for(int ii = 0;ii<diaTree.Count;ii++)
+        {
+            for(int nodecount = 0; nodecount < diaTree[ii].Count;ii++)
+            {
+                for (int randcount = 0; randcount < Random.Range(0, 4); randcount++)
+                {
+                    nodes = new MarkovNodes<Dialouge_nodes>();
+                    if (ii == 0)
+                    {
+                        nodes = diaTree[ii + 1][Random.Range(0, diaTree[ii+1].Count)];
+                    }
+                    else
+                    {
+                        nodes = diaTree[ii + Random.Range(-1,1)][Random.Range(0, diaTree[ii + 1].Count)];
+                    }
+                    diaTree[ii][nodecount].createNode(nodes);
+                }
+            }
+        }
+    }
+
+    public void assignlevel()
+    {
+        for(int ii =0; ii<levels;ii++)
+        {
+            diaTree.Add(dialogue);
+            dialogue = new List<MarkovNodes<Dialouge_nodes>>();
+        }
+    }
+
+    public void filltree()
+    {
+        Dialouge_nodes temp = new Dialouge_nodes();
+        StreamReader reader = new StreamReader("Assets/Scripts/Node Data/node.txt");
+        if(reader == null)
+        {
+            File.Create("Assets/Scripts/Node Data/node.txt");
+        }
+        while (!reader.EndOfStream)
+        {
+            string nodetxt = reader.ReadToEnd();
+            string[] notes  = nodetxt.Split(',');
+            foreach(string note in notes)
+            {
+                nodes = new MarkovNodes<Dialouge_nodes>();
+                temp.dialogue = note;
+                nodes.data = temp;
+                int num;
+                num = note[note.Length - 1] - '0';
+                Debug.Log(num);
+                if(diaTree[num - 1] == null)
+                {
+                    dialogue = new List<MarkovNodes<Dialouge_nodes>>();
+                    diaTree.Add(dialogue);
+                }
+                diaTree[num - 1].Add(nodes);
+                
+            }
+        }
+    }
+}
+
---
+++ DBF/Assets/Scripts/MarkovNodes.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MarkovNodes<T> : MonoBehaviour {
+    static int num = 0;
+    public T data;
+    protected float percentage;
+    public bool balanced = false;
+    private Vector2 range = new Vector2();
+    protected List<MarkovNodes<T>> Nodes = new List<MarkovNodes<T>>();
+    private void Awake()
+    {
+        num += 1;
+    }
+    private void balancePercentages(List<MarkovNodes<T>> node)
+    {
+        if (!balanced)
+        {
+            float total = 0;
+            for (int ii = 0; ii < node.Count; ii++)
+            {
+                total += node[ii].getPercentages();
+            }
+            for (int ii = 0; ii < node.Count; ii++)
+            {
+                node[ii].setPercentage(node[ii].getPercentages() / total);
+            }
+            balanced = true;
+        }
+    }
+
+    private bool checkEqual(List<MarkovNodes<T>> nodes)
+    {
+        float temp = nodes[0].getPercentages();
+        for(int ii = 0;ii<nodes.Count;ii++)
+        {
+            if(!(temp == nodes[ii].getPercentages()))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private MarkovNodes<T> randNode(List<MarkovNodes<T>> node)
+    {
+        float rangestart = 0;
+        for(int ii = 0;ii < node.Count;ii++)
+        {
+            node[ii].range.Set(rangestart, rangestart + percentage);
+            rangestart += percentage;
+        }
+        float randnum = Random.Range(0, 100);
+        randnum /= 100;
+        for(int ii = 0;ii<node.Count;ii++)
+        {
+            if(randnum> node[ii].range.x && randnum < node[ii].range.y)
+            {
+                return node[ii];
+            }
+        }
+        Debug.Log("Randnum broke!");
+        return null;
+    }
+
+    public float getPercentages()
+    {
+        return percentage;
+    }
+
+    public T getData()
+    {
+        return data;
+    }
+
+    public MarkovNodes<T> getNode(T nodeData)
+    {
+        for(int ii = 0;ii < Nodes.Count;ii++)
+        {
+            if(Nodes[ii].getData().Equals(nodeData))
+            {
+                return Nodes[ii];
+            }
+        }
+        Debug.Log("Node not found");
+        return null;
+    }
+
+    public void setPercentage(float percentage)
+    {
+        this.percentage = percentage;
+        balanced = false;
+    }
+
+    public void setData(T data)
+    {
+        this.data = data;
+        balanced = false;
+    }
+
+    public void createNode(MarkovNodes<T> Node)
+    {
+        Nodes.Add(Node);
+        balanced = false;
+    }
+
+    public void deleteNode(MarkovNodes<T> Node)
+    {
+        if(Nodes.Contains(Node))
+        {
+            Nodes.Remove(Node);
+        }
+        balanced = false;
+    }
+
+    public void deleteNode(T data)
+    {
+        Nodes.Remove(getNode(data));
+        balanced = false;
+    }
+
+    public MarkovNodes<T> chooseNode()
+    {
+        if (Nodes.Count != 0)
+        {
+            if(checkEqual(Nodes)) { return Nodes[Random.Range(0, Nodes.Count)]; }
+
+            balancePercentages(Nodes);
+            return randNode(Nodes);
+        }
+        else
+        {
+            Debug.Log("No Nodes");
+            return null;
+        }
+    }
+
+    override
+    public string ToString()
+    {
+        string print = ""+ num + "( ";
+        for(int ii = 0;ii<Nodes.Count;ii++)
+        {
+            print = print + Nodes[ii].ToString();
+        }
+
+       return print = print + ")";
+    }
+}
