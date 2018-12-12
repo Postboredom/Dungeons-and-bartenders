16ebbd3ef445d7d33bec944874ff8b42f26c0c4a
Jared <jarlong@csumb.edu>
2018-12-12 00:23:06 -0800

    Project File Merge (Buggy)

---
+++ DBF/Assets/Scripts/BarStatsHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarStatsHandler : MonoBehaviour {
+
+
+    public int totalGold;
+    public float totalBarAttractiveness;
+    public bool secondStory;
+
+    [SerializeField]
+    private Text goldAmountText;
+    [SerializeField]
+    private Text barAttractivenessText;
+
+	// Use this for initialization
+	void Start () {
+
+        totalGold = 250;
+        totalBarAttractiveness = 10;
+        secondStory = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        goldAmountText.text = totalGold.ToString();
+        BarRating();
+	}
+
+    void BarRating()
+    {
+        if (totalBarAttractiveness >= 20)
+        {
+            barAttractivenessText.text = "Unkempt Hovel";
+        }
+        if (totalBarAttractiveness >= 45)
+        {
+            barAttractivenessText.text = "Grubby Taproom";
+        }
+        if (totalBarAttractiveness >= 80)
+        {
+            barAttractivenessText.text = "Tolerable Tavern";
+        }
+        if (totalBarAttractiveness >= 110)
+        {
+            barAttractivenessText.text = "Noteworthy Ale House";
+        }
+        if (totalBarAttractiveness >= 140)
+        {
+            barAttractivenessText.text = "Impressive Inn";
+        }
+        if (totalBarAttractiveness >= 160)
+        {
+            barAttractivenessText.text = "Exemplary Establishment";
+        }
+        if (totalBarAttractiveness >= 205)
+        {
+            barAttractivenessText.text = "Phenomenal Bistro";
+        }
+        if (totalBarAttractiveness >= 250)
+        {
+            barAttractivenessText.text = "6 Star Bar";
+        }
+    }
+}
---
+++ DBF/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MenuHandler : MonoBehaviour {
+
+    public GameObject areaDecoratorUI;
+    bool visibleUI = false;
+    public GameObject playerObject;
+    public string currentPlaceableObjectName;
+    public bool isObjectPlaced;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MenuToggler();
+    }
+
+    void MenuToggler()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (visibleUI == false)
+            {
+                MenuToggleOn();
+            }
+            else 
+            {
+                MenuToggleOff();
+            }
+         
+        }
+    }
+    public void MenuToggleOn()
+    {
+        visibleUI = true;
+      
+      
+        
+        areaDecoratorUI.SetActive(true);
+        playerObject.GetComponent<FirstPersonController>().enabled = false;
+        playerObject.GetComponent<FirstPersonController>().m_MouseLook.lockCursor = false;
+        Cursor.lockState = CursorLockMode.None;
+        //Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        Debug.Log(Cursor.visible);
+        Debug.Log(playerObject.GetComponent<FirstPersonController>().m_MouseLook.lockCursor);
+    }
+    public void MenuToggleOff()
+    {
+        playerObject.GetComponent<FirstPersonController>().enabled = true;
+        //playerObject.GetComponent<FirstPersonController>().m_MouseLook.lockCursor = true;
+        areaDecoratorUI.SetActive(false);
+        Cursor.visible = false;
+        visibleUI = false;
+    }
+    public void ObjectButtonPressed()
+    {
+        currentPlaceableObjectName = EventSystem.current.currentSelectedGameObject.name;
+        isObjectPlaced = false;
+    }
+}
